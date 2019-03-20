2a188315b919287b85d85ee4fcf03b74b2ea9e10
ZarelLast <lastzarel@gmail.com>
2019-03-20 09:48:23 +0700

    sama

--- src/gofood/components/input.jsx
+++ src/gofood/components/input.jsx
@@ -47,6 +47,9 @@ export default class GofoodInput extends Component {
 
     axios
       .post('https://larapi-react.herokuapp.com/api/gofood', {
+        headers: {
+          'Access-Control-Allow-Origin': 'https://stupefied-colden-26ba04.netlify.com',
+        },
         'title': this.state.title,
         'image': this.state.image,
         'price': this.state.price,
